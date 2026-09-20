f7c0691e8dcba11e37ce6380c1aa8f811bfcca1b
DmitryVas123 <vasiletsdima06@gmail.com>
2026-09-20 22:57:45 +0300

    Start EDA

--- eda.py
+++ eda.py
@@ -1,7 +1,12 @@
 import numpy as np
+from matplotlib import pyplot as plt
 from scipy import stats
-
+import seaborn as sns
 import data_loader as dl
+
+sns.set_palette("husl")
+plt.style.use('seaborn-v0_8')
+
 
 df = dl.load_data()
 dl.data_info(df)
@@ -17,3 +22,10 @@ for col in numeric_cols:
     skewness = stats.skew(df[col].dropna())
     kurtosis = stats.kurtosis(df[col].dropna())
     print(f"{col}: Skewness = {skewness:.3f}, Kurtosis = {kurtosis:.3f}")
+
+print("\nTarget Value")
+plt.figure(figsize=(10, 8))
+sns.boxplot(x='Engine Condition', y='Lub oil pressure', data=df)
+plt.title('Engine Condition: Lub oil pressure')
+plt.tight_layout()
+plt.show()
